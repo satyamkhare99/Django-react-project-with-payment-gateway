418a70050f1aef81609340196a24f13021d3267c
Satyam Khare <satyamkhare05071999@gmail.com>
2020-10-19 23:19:39 +0530

    initial commit

---
+++ lcodev/ecom/api/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+from api.user.models import CustomUser
+
+class Migration(migrations.Migration):
+    def seed_data(apps, schema_editor):
+        user = CustomUser(name="Satyam Khare",
+                        email="satyamkhare05071999@gmail.com",
+                        is_staff=True,
+                        is_superuser=True,
+                        phone="8959082064",
+                        gender="Male"
+                         )
+        user.set_password("12345")
+        user.save()
+    dependencies=[
+
+    ]
+
+    operations=[
+        migrations.RunPython(seed_data),
+    ]
